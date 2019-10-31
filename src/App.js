de0b472d816ba6123503d2dddbc79620ba25529b
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2019-10-30 23:33:19 -0400

    input works

--- src/App.js
+++ src/App.js
@@ -1,16 +1,63 @@
 import React from 'react';
-
+import TodoForm from './components/TodoComponents/TodoForm';
+import TodoList from './components/TodoComponents/TodoList';
+const data = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+]
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+    super();
+    this.state = {
+      todo: data,
+      newItem: ""
+    };
+  }
+
+  addItem = (task) => {
+    const newItem = {
+      task: task,
+      id: Date.now(),
+      completed: false
+    };
+    this.setState({
+      todo: [...this.state.todo, newItem]
+    });
+  }
+
+  deleteItem = () => {
+    this.setState({
+      todo: this.state.todo.filter(item => {
+        return item.done !== true;
+      })
+    })
+  }
+  
+  
+  // onSubmit()
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+      <TodoForm addItem={this.addItem} />
+      <TodoList todo={this.state.todo} />
       </div>
     );
   }
 }
 
 export default App;
+
+// toggleDone = (id) => {
+//   this.setState({
+//     todo: this.state.todo.map()
+//   })
+// }
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+const TodoForm = (props) => {
+    const [newItem, setNewItem] = useState();
+
+    const handleChange = (e) => {
+        setNewItem(e.target.value);
+
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        props.addItem(newItem);
+        setNewItem('')
+
+      }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input 
+                type="text"
+                id="item"
+                name="item"
+                value={newItem}
+                onChange={handleChange} />
+
+                <button>Add</button>
+            </form>
+        </div>
+    )
+}
+export default TodoForm
